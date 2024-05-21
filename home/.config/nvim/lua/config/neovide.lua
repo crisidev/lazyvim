1f4db4878ae11c3d8759dd3c3b6be645d6ae08ed
Bigo <1781140+crisidev@users.noreply.github.com>
2024-05-21 16:57:27 +0100

    Disable blur in neovide

--- home/.config/nvim/lua/config/neovide.lua
+++ home/.config/nvim/lua/config/neovide.lua
@@ -16,8 +16,8 @@ if vim.g.neovide then
     vim.g.neovide_padding_bottom = 0
     vim.g.neovide_padding_right = 0
     vim.g.neovide_padding_left = 0
-    vim.g.neovide_floating_blur_amount_x = 2.0
-    vim.g.neovide_floating_blur_amount_y = 2.0
+    vim.g.neovide_floating_blur_amount_x = 0.0
+    vim.g.neovide_floating_blur_amount_y = 0.0
     vim.g.neovide_transparency = 1.0
     vim.g.neovide_window_blurred = true
     vim.g.neovide_scroll_animation_length = 0.0
