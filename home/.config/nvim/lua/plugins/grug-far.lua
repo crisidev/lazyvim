78f51ac1d5019778f708aeee44c99b8b1ecbf8af
Bigo <1781140+crisidev@users.noreply.github.com>
2024-07-21 16:46:01 +0200

    Fix grug-far keybindings

--- home/.config/nvim/lua/plugins/grug-far.lua
+++ home/.config/nvim/lua/plugins/grug-far.lua
@@ -2,21 +2,23 @@ return {
     "MagicDuck/grug-far.nvim",
     opts = { headerMaxWidth = 80 },
     cmd = "GrugFar",
-    keys = {
-        {
-            "<leader>R",
-            function()
-                local is_visual = vim.fn.mode():lower():find("v")
-                if is_visual then -- needed to make visual selection work
-                    vim.cmd([[normal! v]])
-                end
-                local grug = require("grug-far");
-                (is_visual and grug.with_visual_selection or grug.grug_far)({
-                    prefills = { filesFilter = "*." .. vim.fn.expand("%:e") },
-                })
-            end,
-            mode = { "n", "v" },
-            desc = "Search and Replace",
-        },
-    },
+    keys = function()
+        return {
+            {
+                "<leader>R",
+                function()
+                    local is_visual = vim.fn.mode():lower():find("v")
+                    if is_visual then -- needed to make visual selection work
+                        vim.cmd([[normal! v]])
+                    end
+                    local grug = require("grug-far");
+                    (is_visual and grug.with_visual_selection or grug.grug_far)({
+                        prefills = { filesFilter = "*." .. vim.fn.expand("%:e") },
+                    })
+                end,
+                mode = { "n", "v" },
+                desc = "Search and Replace",
+            },
+        }
+    end,
 }
