eb806ef64f1a8b3dd0bc98510d0838af16a3ddac
Bigo <1781140+crisidev@users.noreply.github.com>
2024-12-13 13:02:24 +0000

    Switch silicon config to pure lua

--- home/.config/nvim/lua/plugins/silicon.lua
+++ home/.config/nvim/lua/plugins/silicon.lua
@@ -3,23 +3,22 @@ return {
     lazy = true,
     cmd = "Silicon",
     config = function()
-        vim.cmd([[
-            let g:silicon = {
-              \   'theme': 'tokyonight_storm',
-              \   'font': 'MonoLisa Nerd Font',
-              \   'background':         '#202228',
-              \   'shadow-color':       '#000000',
-              \   'line-pad': 0,
-              \   'pad-horiz': 20,
-              \   'pad-vert': 20,
-              \   'shadow-blur-radius': 0,
-              \   'shadow-offset-x': 0,
-              \   'shadow-offset-y': 0,
-              \   'line-number': v:true,
-              \   'round-corner': v:true,
-              \   'window-controls': v:true,
-              \   'output': "~/Pictures/vim-screenshots/silicon-{time:%Y-%m-%d-%H%M%S}.png",
-              \ }
-        ]])
+        local home = vim.env.HOME
+        vim.g.silicon = {
+            theme = home .. "/.config/bat/themes/tokyonight.nvim/extras/sublime/tokyonight_storm.tmTheme",
+            background = "#202228",
+            font = "MonoLisa Nerd Font",
+            ["line-number"] = true,
+            ["line-pad"] = 0,
+            output = home .. "/Pictures/Screenshots/neovim-{time:%Y-%m-%d-%H%M%S}.png",
+            ["pad-horiz"] = 20,
+            ["pad-vert"] = 20,
+            ["round-corner"] = true,
+            ["shadow-blur-radius"] = 0,
+            ["shadow-color"] = "#000000",
+            ["shadow-offset-x"] = 0,
+            ["shadow-offset-y"] = 0,
+            ["window-controls"] = true,
+        }
     end,
 }
