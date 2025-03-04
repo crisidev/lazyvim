41430b011927df076e2a7f35b268eca5bcd35987
Bigo <1781140+crisidev@users.noreply.github.com>
2025-03-04 10:22:07 +0000

    Remove gitlab plugin
    Signed-off-by: Bigo <1781140+crisidev@users.noreply.github.com>

--- home/.config/nvim/lua/config/options.lua
+++ home/.config/nvim/lua/config/options.lua
@@ -7,7 +7,6 @@ vim.g.loaded_perl_provider = 0
 vim.g.loaded_ruby_provider = 0
 vim.g.transparent = false
 vim.g.ai_plugin = false
-vim.g.gitlab = false
 
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
