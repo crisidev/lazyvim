3dccf01d869875f22582e64e35ac16b127218258
Matteo Bigoi <bigo@crisidev.org>
2026-03-18 14:51:18 +0000

    Add new mdx syntax

--- home/.config/nvim/lua/plugins/syntaxes.lua
+++ home/.config/nvim/lua/plugins/syntaxes.lua
@@ -1,4 +1,9 @@
 return {
     { "Glench/Vim-Jinja2-Syntax", event = "VeryLazy" },
     { "earthly/earthly.vim", event = "VeryLazy" },
+    {
+        "davidmh/mdx.nvim",
+        dependencies = "nvim-treesitter/nvim-treesitter",
+        event = "VeryLazy",
+    },
 }
